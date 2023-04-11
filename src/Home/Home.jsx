5019c64dea97df40bee07520e20189f0fad5547c
GOATAFI <projectexecutiveofpm@gmail.com>
2023-04-11 22:22:08 +0600

    second commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = (tshirt) => {
+        const newCart = [...cart, tshirt]
+        setCart(newCart)
+    }
+    const handleRemoveFromCart = id => {
+      const remaining = cart.filter(ts => ts._id !== id)
+      setCart(remaining)
+    }
     const tshirts = useLoaderData();
     return (
-        <div>
-            <h1>This is home . We have {tshirts.length} T-Shirts</h1>
+        <div className='home-container'>
+            <div className="t-shirt-container">
+                {
+                    tshirts.map(tshirt => <Tshirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    >
+                    </Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    cart={cart}
+                ></Cart>
+            </div>
+
+
         </div>
     );
 };
